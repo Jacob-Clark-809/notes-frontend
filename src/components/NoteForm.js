ab23fe5651cd7b487a92af91e6f2593a406d2b53
Jacob Clark <jacobclark1198@gmail.com>
2023-09-06 01:28:32 +0100

    everything

---
+++ src/components/NoteForm.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+const NoteForm = ({ createNote }) => {
+  const [newNote, setNewNote] = useState('');
+
+  const addNote = (event) => {
+    event.preventDefault();
+    createNote({
+      content: newNote,
+      important: true
+    });
+
+    setNewNote('');
+  };
+
+  return (
+    <>
+      <h2>Create a new note</h2>
+
+      <form onSubmit={addNote}>
+        <input
+          id='create-note'
+          value={newNote}
+          onChange={event => setNewNote(event.target.value)} />
+        <button type="submit">save</button>
+      </form>
+    </>
+  );
+};
+
+export default NoteForm;
